ffe6482cfab82c2bbe184eba6273768cdcf20f20
Krishi Sharma <54914189+krishisharma45@users.noreply.github.com>
2020-02-01 22:18:34 -0800

    Add elevator front-end components and express server
    Added express server to calculate the number of stops that the elevator must take in order to service the number of people given the data provided by the user in the browser.

--- app.js
+++ app.js
@@ -2,21 +2,48 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}))
-
-//var answer = a + b;
 
 app.get("/", function (request, response){
   response.sendFile(__dirname + "/elevator.html");
 });
 
-app.post("/elevator.html", function(request, response){
-  var num1 = Number(request.body.maxPeople);
-  var num2 = Number(request.body.maxWeight);
-  var weight = new Array(request.body.A);
-  var floor = new Array(request.body.B);
-  var answer = num1 + num2;
-  response.send("The result is " + answer);
+app.post("/", function(request, response){
+  let maxPeople = Number(request.body.maxPeople);
+  let maxWeight = Number(request.body.maxWeight);
+  let weight = request.body.A;
+  let floor = request.body.B;
+  let weightQueue = weight.split(',').map(Number);
+  let floorQueue = floor.split(',').map(Number);
+
+  //elevator.js logic
+  function calculateStops(maxPeople, maxWeight, weightQueue, floorQueue){
+    let weightStack = [];
+    let floorStack = [];
+    let stops = 0;
+    while (weightQueue.length){
+      totalWeight = weightStack.reduce((a, b) => a + b, 0) + weightQueue[0];
+      if ((weightStack.length < maxPeople) && (totalWeight <= maxWeight)){
+        weightStack.push(weightQueue.shift());
+        floorStack.push(floorQueue.shift());
+      }
+      else{
+        //console.log(weightStack);
+        stops+=new Set(floorStack).size + 1;
+        weightStack = [];
+        floorStack = [];
+        //console.log(stops);
+      }
+    }
+    if (floorStack.length != 0){
+      stops+=new Set(floorStack).size + 1;
+      return stops;
+    }
+    return stops;
+  }
+  stops = calculateStops(maxPeople, maxWeight, weightQueue, floorQueue);
+  response.send("The elevator stops " + stops + " times.");
 });
 
 app.listen(process.env.PORT || 3000, function() {
--- elevator-recursive.js
+++ elevator-recursive.js
@@ -1,24 +1,26 @@
+let weightStack = [];
+let floorStack = [];
+let stops = 0;
+
 function calculateStops(maxPeople, maxWeight, weightQueue, floorQueue){
-  let weightStack = [];
-  let floorStack = [];
-  let stops = 0;
-  while (weightQueue.length){
+  if (floorQueue.length === 0){
+    stops+=new Set(floorStack).size + 1;
+    return stops;
+  }
+  else{
     totalWeight = weightStack.reduce((a, b) => a + b, 0) + weightQueue[0];
     if ((weightStack.length < maxPeople) && (totalWeight <= maxWeight)){
       weightStack.push(weightQueue.shift());
       floorStack.push(floorQueue.shift());
     }
     else{
-      stops+=new Set(floorStack).size + 1;
+      let floorSet = new Set(floorStack);
+      stops+=floorSet.size + 1;
       weightStack = [];
       floorStack = [];
     }
+    return calculateStops(floorQueue.shift());
   }
-  if (floorStack.length != 0){
-    stops+=new Set(floorStack).size + 1;
-    return stops;
-  }
-  return stops;
 }
 
 let A = [200, 200, 30, 40, 60, 60, 30, 50];
